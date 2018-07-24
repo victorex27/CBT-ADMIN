26e2da02fe78ef8b0333d4a44d5cf07e14a244cd
Amaobi Victor <aobikobe@gmail.com>
2018-07-24 08:45:38 +0100

    db

--- src/StudentAssignmentAnswerFXMLController.java
+++ src/StudentAssignmentAnswerFXMLController.java
@@ -4,6 +4,12 @@
  * and open the template in the editor.
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -45,7 +51,7 @@ public class StudentAssignmentAnswerFXMLController implements Initializable {
     }
 
     @FXML
-    public void download(ActionEvent evt) throws SQLException,ClassNotFoundException {
+    public void download(ActionEvent evt) throws SQLException,ClassNotFoundException, FileNotFoundException, IOException {
 
         Connection connection = SimpleConnection.getConnection();
         
@@ -61,6 +67,30 @@ public class StudentAssignmentAnswerFXMLController implements Initializable {
         ResultSet resultSet = pStatement.executeQuery();
 
         while (resultSet.next()) {
+            
+            String name = resultSet.getString("name");
+                
+
+                
+                File file = new File(name);
+
+                InputStream is = resultSet.getBinaryStream("file");
+                OutputStream os = new FileOutputStream(file);
+
+                byte[] content = new byte[1024];
+                int size = 0;
+                while ((size = is.read(content)) != -1) {
+                    os.write(content, 0, size);
+                }
+
+                os.close();
+                is.close();
+            
+                    //Desktop.getDesktop().open(file);
+                    
+                    
+                    file.renameTo( new File(System.getProperty("user.home")+"//Documents//"+file.getName()));
+                    
         }
     }
 
